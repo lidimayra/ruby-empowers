5d7665d6073eee69f5a82b709398ae450fd7329a
Lidiane Taquehara <lmt-dev.chunk990@passinbox.com>
2025-05-03 18:25:56 +0100

    Adiciona o seeds.rb
    Cria alguns exemplos de livros no banco de dados para facilitar
    exemplos de consultas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,62 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Book.create!(
+  title: "Desesterro",
+  publisher: "Record",
+  language: "Portuguese",
+  quantity: 10,
+)
+
+Book.create!(
+  title: "O habitante das falhas subterrâneas",
+  publisher: "Oito e meio",
+  language: "Portuguese",
+  quantity: 0,
+)
+
+Book.create!(
+  title: "Convite à Filosofia",
+  publisher: "Ática",
+  language: "Portuguese",
+  quantity: 9
+)
+
+Book.create!(
+  title: "Fim",
+  publisher: "Companhia das Letras",
+  language: "Portuguese",
+  quantity: 6
+)
+
+Book.create!(
+  title: "Quarto de despejo - Diário de uma favelada",
+  publisher: "Ática",
+  language: "Portuguese",
+  quantity: 0
+)
+
+Book.create!(
+  title: "Half of a Yellow Sun",
+  publisher: "4th Estate",
+  language: "English",
+  quantity: 3
+)
+
+Book.create!(
+  title: "Pride and Prejudice",
+  publisher: "Wordsworth Editions Ltd",
+  language: "English",
+  quantity: 7
+)
+
+Book.create!(
+  title: "Wuthering Heights",
+  publisher: "Wordsworth Editions Ltd",
+  language: "English",
+  quantity: 0
+)
+
+Book.create!(
+  title: "The Color Purple",
+  publisher: "W&N",
+  language: "English",
+  quantity: 5
+)
